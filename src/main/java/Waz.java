b6c2819b8c59c988e511fd8c33925f167452bcbe
Derekqua <derekqua8@gmail.com>
2025-08-21 20:51:48 +0800

    Add echo command and exit on 'bye'
    Chatbot can echo back any commaned entered by the user, exit when the user types 'bye'
    
    Enhances user engagement.

--- src/main/java/Waz.java
+++ src/main/java/Waz.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class Waz {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Greet User
         greet();
-        exit();
+
+        // Loop
+        while (true) {
+            String input = scanner.nextLine();
+
+            // Input is bye, break loop
+            if (input.equals("bye")) {
+                exit();
+                break;
+            }
+
+            // Echo command entered by the user
+            echo(input);
+
+        }
+
+        scanner.close();
     }
 
     private static void exit() {
@@ -16,6 +37,12 @@ public class Waz {
     }
     private static void horizontalLine() {
         System.out.println("----------------------------------------------");
+    }
+
+    private static void echo(String input) {
+        horizontalLine();
+        System.out.println(input);
+        horizontalLine();
     }
 
 }
