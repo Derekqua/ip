3d1d8cafed82a6343f09ac93d09cead6e33a8051
Derekqua <derekqua8@gmail.com>
2025-08-28 22:18:03 +0800

    Add capability to understand dates and times

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,19 +1,29 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Locale;
+
 public class Deadline extends Task {
 
-    protected String by;
+    private LocalDateTime by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String byString) {
         super(description);
-        this.by = by;
+        try {
+            this.by = LocalDateTime.parse(byString, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+        } catch (DateTimeParseException e) {
+            System.err.println("Invalid date/time format. Please use 'yyyy-MM-dd HHmm' (e.g., 2019-10-15 1800).");
+        }
     }
 
     @Override
     public String toDataString() {
-        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " +
+                by.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM dd yyyy HH:mm")) + ")";
     }
 }
